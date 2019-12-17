342f282a3e4cc25f209b925ff281d8eeddae204c
kerrmcmaster <58255108+kerrmcmaster@users.noreply.github.com>
2019-12-17 11:23:19 +0100

    Update CODE_01.py
    final code

--- CODE_01.py
+++ CODE_01.py
@@ -10,11 +10,11 @@ import json
 def main():
     # setting data values
     data_list = []
-    value1 = 0
+    value1 = 1
 
-    stature_list = [1500.0, 1600.0, 1700.0, 1800.0, 1900.0]
-    weight_list = [50.0, 60.0, 75.0, 85.0, 95.0]
-    waist_list = [650.0, 750.0, 850.0, 1000.0, 1150.0]
+    stature_list = [1550, 1600, 1700, 1800, 1900]
+    weight_list = [50, 60, 75, 85, 95]
+    waist_list = [650, 750, 850, 1000, 1150]
     hatch_list = [1, 2, 3, 4, 5, 6, 7]
     skeg_list = [True, False]
     rudder_list = [True, False]
@@ -128,57 +128,9 @@ def main():
 
         nErrs8 = theSession.UpdateManager.DoUpdate(markId8)
 
-    # Exports data to file in .STL (provides a log of change also):
-    def save_stl(value1):
-
-        markId10 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "STL Export")
-
-        # STL set up:
-        sTLCreator1 = theSession.DexManager.CreateStlCreator()
-        sTLCreator1.AutoNormalGen = True
-        sTLCreator1.ChordalTol = 0.080000000000000002
-        sTLCreator1.AdjacencyTol = 0.080000000000000002
-
-        # Output path:
-        sTLCreator1.OutputFile = f"D:\\path\\Kayak_{str(value1)}.stl"
-
-        # Adding Bodies to STL export:
-        objects1 = [NXOpen.NXObject.Null] * 13
-        body1 = workPart.Bodies.FindObject("EXTRUDE(78)")
-        objects1[0] = body1
-        body2 = workPart.Bodies.FindObject("EXTRUDE(46)")
-        objects1[1] = body2
-        body3 = workPart.Bodies.FindObject("EXTRUDE(71)")
-        objects1[2] = body3
-        body4 = workPart.Bodies.FindObject("EXTRUDE(106)")
-        objects1[3] = body4
-        body5 = workPart.Bodies.FindObject("EXTRUDE(54)")
-        objects1[4] = body5
-        body6 = workPart.Bodies.FindObject("EXTRUDE(44)")
-        objects1[5] = body6
-        body7 = workPart.Bodies.FindObject("EXTRUDE(52)")
-        objects1[6] = body7
-        body8 = workPart.Bodies.FindObject("EXTRUDE(100)")
-        objects1[7] = body8
-        body9 = workPart.Bodies.FindObject("EXTRUDE(60)")
-        objects1[8] = body9
-        body10 = workPart.Bodies.FindObject("EXTRUDE(92)")
-        objects1[9] = body10
-        body11 = workPart.Bodies.FindObject("EXTRUDE(62)")
-        objects1[10] = body11
-        body12 = workPart.Bodies.FindObject("EXTRUDE(43)")
-        objects1[11] = body12
-        body13 = workPart.Bodies.FindObject("EXTRUDE(45)")
-        objects1[12] = body13
-        added1 = sTLCreator1.ExportSelectionBlock.Add(objects1)
-
-        nXObject1 = sTLCreator1.Commit()
-        sTLCreator1.Destroy()
-
-        nErrs10 = theSession.UpdateManager.DoUpdate(markId10)
-
 # -------------------------------------------------------------------------------------------------------------------- #
 # OUTPUT STAGE
+# Cycles through all variations and defines values for the CAD model and JSON file. Then exports a file, image and JSON
 
     for weight in weight_list:
         for waist in waist_list:
@@ -189,29 +141,41 @@ def main():
                         # Marks point for rollback if errors encountered
                         markId1 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible,
                                                          "Set Width + Length")
-
                         if weight < 55.0:
                             kayak_width = 480.0
                             kayak_length = 4500.0
+
+                            weight_config = "<55"
 
                         elif 55.0 <= weight <= 70.0:
                             kayak_width = 500.0
                             kayak_length = 4500.0
 
+                            weight_config = "55-70"
+
                         elif 71.0 <= weight <= 80.0:
                             kayak_width = 500.0
                             kayak_length = 5000.0
+
+                            weight_config = "71-80"
 
                         elif 81.0 <= weight <= 90.0:
                             kayak_width = 550.0
                             kayak_length = 5000.0
 
+                            weight_config = "81-90"
+
                         elif weight > 90.0:
                             kayak_width = 600.0
                             kayak_length = 5500.0
+
+                            weight_config = ">90"
+
                         else:
                             kayak_length = 5500.0
                             kayak_width = 600.0
+
+                            weight_config = "Null"
 
                         nErrs1 = theSession.UpdateManager.DoUpdate(markId1)
 
@@ -219,23 +183,30 @@ def main():
 
                         if waist < 700.0 and kayak_width >= 480.0:
                             cockpit_width = 400.0
+                            waist_config = "<70"
 
                         elif 701.0 <= waist <= 800.0 and kayak_width >= 500.0:
                             cockpit_width = 430.0
+                            waist_config = "70-80"
 
                         elif 801.0 <= waist <= 950.0 and kayak_width >= 550.0:
                             cockpit_width = 450.0
+                            waist_config = "81-95"
 
                         elif 951.0 <= waist <= 1100.0 and kayak_width >= 550.0:
                             cockpit_width = 470.0
+                            waist_config = "96-110"
 
                         elif waist > 1100.0 and kayak_width >= 600.0:
                             cockpit_width = 490.0
+                            waist_config = ">110"
 
                         elif waist > 1100.0 and kayak_width < 600.0:
                             cockpit_width = 430.0
+                            waist_config = ">110"
                         else:
                             cockpit_width = 400.0
+                            waist_config = "<70"
 
                         nErrs2 = theSession.UpdateManager.DoUpdate(markId2)
 
@@ -280,7 +251,7 @@ def main():
                             hatch_config = "Day Hatch on right side + Rear storage"
 
                         elif hatch == 5:  # 5 = day hatch LHS + Rear Storage + Front Storage
-                            hatch_config = "Day Hatch on right side + Rear storage + Front storage"
+                            hatch_config = "Day Hatch on left side + Rear storage + Front storage"
 
                         elif hatch == 6:  # 6 = day hatch RHS + Rear Storage + Front Storage
                             hatch_config = "Day Hatch on right side + Rear storage + Front storage"
@@ -314,31 +285,31 @@ def main():
                             NXOpen.NXObject.Null)
 
                         bodies1 = [NXOpen.Body.Null] * 13
-                        body1 = workPart.Bodies.FindObject("EXTRUDE(78)")
+                        body1 = workPart.Bodies.FindObject("EXTRUDE(60)")
                         bodies1[0] = body1
-                        body2 = workPart.Bodies.FindObject("EXTRUDE(52)")
+                        body2 = workPart.Bodies.FindObject("EXTRUDE(78)")
                         bodies1[1] = body2
-                        body3 = workPart.Bodies.FindObject("EXTRUDE(44)")
+                        body3 = workPart.Bodies.FindObject("EXTRUDE(54)")
                         bodies1[2] = body3
-                        body4 = workPart.Bodies.FindObject("EXTRUDE(106)")
+                        body4 = workPart.Bodies.FindObject("EXTRUDE(92)")
                         bodies1[3] = body4
                         body5 = workPart.Bodies.FindObject("EXTRUDE(71)")
                         bodies1[4] = body5
-                        body6 = workPart.Bodies.FindObject("EXTRUDE(43)")
+                        body6 = workPart.Bodies.FindObject("EXTRUDE(100)")
                         bodies1[5] = body6
-                        body7 = workPart.Bodies.FindObject("EXTRUDE(60)")
+                        body7 = workPart.Bodies.FindObject("EXTRUDE(106)")
                         bodies1[6] = body7
-                        body8 = workPart.Bodies.FindObject("EXTRUDE(45)")
+                        body8 = workPart.Bodies.FindObject("EXTRUDE(43)")
                         bodies1[7] = body8
-                        body9 = workPart.Bodies.FindObject("EXTRUDE(54)")
+                        body9 = workPart.Bodies.FindObject("EXTRUDE(44)")
                         bodies1[8] = body9
-                        body10 = workPart.Bodies.FindObject("EXTRUDE(62)")
+                        body10 = workPart.Bodies.FindObject("EXTRUDE(45)")
                         bodies1[9] = body10
-                        body11 = workPart.Bodies.FindObject("EXTRUDE(92)")
+                        body11 = workPart.Bodies.FindObject("EXTRUDE(46)")
                         bodies1[10] = body11
-                        body12 = workPart.Bodies.FindObject("EXTRUDE(100)")
+                        body12 = workPart.Bodies.FindObject("EXTRUDE(62)")
                         bodies1[11] = body12
-                        body13 = workPart.Bodies.FindObject("EXTRUDE(46)")
+                        body13 = workPart.Bodies.FindObject("EXTRUDE(52)")
                         bodies1[12] = body13
                         bodyDumbRule1 = workPart.ScRuleFactory.CreateRuleBodyDumb(bodies1, True)
 
@@ -367,34 +338,70 @@ def main():
                         nErrs9 = theSession.UpdateManager.DoUpdate(markId9)
 
                         for stature in stature_list:
+
+                            if stature == 1550:
+                                stature_config = "<155"
+                            elif stature == 1600:
+                                stature_config = "155-165"
+                            elif stature == 1700:
+                                stature_config = "166-175"
+                            elif stature == 1800:
+                                stature_config = "176-185"
+                            elif stature == 1900:
+                                stature_config = ">185"
+                            else:
+                                stature_config = "Null"
+
                             data = {
                                 "variant": value1,          # Float
-                                "stature": stature,         # Float
-                                "weight": weight,           # Float
+                                "stature": stature_config,  # String
+                                "weight": weight_config,    # String
                                 "length": kayak_length,     # Float
                                 "width": kayak_width,       # Float
-                                "waist": waist,             # Float
+                                "waist": waist_config,      # String
                                 "skeg": skeg_config,        # String
                                 "rudder": rudder_config,    # String
                                 "hatch": hatch_config,      # String
                                 "volume": volume,           # Float
-                                "cad": f"https://firebasestorage.googleapis.com/v0/b/bucketname.appspot.com/o/kayak_{value1}.stl?alt=media"
-                                }
+                                "cad": f"https://firebasestorage.googleapis.com/v0/b/{bucket_name}.appspot.com/o/Kayak_{value1}.stp?alt=media",
+                                "image": f"https://firebasestorage.googleapis.com/v0/b/{bucket_name}.appspot.com/o/Kayak_{value1}.png?alt=media"
+                            }
 
                             data_list.append(data)
 
                             # Triggers file export
-                            save_stl(value1)
-                            value1 += 1
+                            markId10 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Visible, "STL Export")
+
+                            # STL set up:
+                            sTLCreator1 = theSession.DexManager.CreateStlCreator()
+                            sTLCreator1.AutoNormalGen = True
+                            sTLCreator1.ChordalTol = 0.080000000000000002
+                            sTLCreator1.AdjacencyTol = 0.080000000000000002
+
+                            # Output STEP file:
+                            partSaveStatus1 = workPart.SaveAs(
+                                f"F:\\{path}\\Kayak_{value1}.stp")
+
+                            partSaveStatus1.Dispose()
+
+                            # Image export:
+                            studioImageCaptureBuilder1 = workPart.Views.CreateStudioImageCaptureBuilder()
+
+                            studioImageCaptureBuilder1.NativeFileBrowser = f"F:\\{path}\\Kayak_{value1}.png"
+
+                            nXObject1 = studioImageCaptureBuilder1.Commit()
+                            studioImageCaptureBuilder1.Destroy()
 
                             markId11 = theSession.SetUndoMark(NXOpen.Session.MarkVisibility.Invisible, "Export JSON")
 
-                            write_path = 'C:path/data.json'
+                            write_path = 'F:/{path}/data.json'
 
                             with open(write_path, 'w', encoding='utf-8') as f:
                                 json.dump(data_list, f, ensure_ascii=False, indent=4)
 
                             nErrs11 = theSession.UpdateManager.DoUpdate(markId11)
+
+                            value1 += 1
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
